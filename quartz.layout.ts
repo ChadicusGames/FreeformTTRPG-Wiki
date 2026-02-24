99d48e487f94f4c10d35ffc69ff6b0451762c21f
ChadicusGames <rayncolbourne@gmail.com>
2026-02-24 17:49:40 -0500

    Quartz sync: Feb 24, 2026, 5:49 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    ,
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
@@ -62,9 +62,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer({
-      title: "Wiki Links",
-    }),
+    Component.Explorer(),
   ],
   right: [],
 }
